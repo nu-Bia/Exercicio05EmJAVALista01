ef3b4ab87a2370198d06904ae6f41af3605d7aa9
Núbia <112019351+nu-Bia@users.noreply.github.com>
2022-12-15 16:26:27 -0300

    Add files via upload

---
+++ Exercicio05.java
@@ -0,0 +1,21 @@
+//5) Escreva um algoritmo para ler dois valores do teclado e exibir como resposta a multiplicação dos valores.
+
+import java.util.Scanner;
+
+public class Exercicio05 {
+
+public static void main (String[]args) {
+
+Scanner sc = new Scanner(System.in);
+
+System.out.println("Digite o primeiro valor");
+
+int n1= sc.nextInt();
+
+System.out.println("Digite o segundo valor que deseja multiplicar");
+
+int n2= sc.nextInt();
+
+System.out.println(n1*n2);
+
+ }}
